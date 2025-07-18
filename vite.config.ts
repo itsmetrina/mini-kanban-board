6ce4b1a8387e38ddba0ec07dd8f20326375dae4c
itsmetrina <trina.sikdar.2101@gmail.com>
2025-07-18 23:29:25 +0530

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,         // 👈 enables describe, it, expect without importing
-		environment: 'jsdom',  // 👈 needed for React DOM tests
-	},
+	plugins: [react()]
 })
